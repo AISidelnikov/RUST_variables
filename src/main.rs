07238aceca54a2c3b35c55460afe9023ee4382a6
Alex <AlexFasMod@mail.ru>
2022-09-02 19:33:04 +0500

    Основные типы данных

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+const MAX_POINT: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("Значение x равно {}", x);
+    let x = 6;
+    println!("Значение x равно {}", x);
+    let x = x * 2;
+    println!("Значение x равно {}", x);
+
+    let space = "   ";
+    let space = space.len();
+    println!("Длина строки space = {}", space);
+
+    let guess: u32 = "42".parse().expect("НЕ являетя числом");
+
+    let x = 2.0;
+    let y: f32 = 3.0;
+
+    let t = true;
+    let f: bool = false;
+    println!("Згачение f = {}", f);
+
+    let z = 'z';
+    let c = 'c';
+    println!("z = {}", z);
 }
